7800ff9650a0936657c4717c22e170c5f1411e4a
dunhanson <dunhanson@gmail.com>
2021-01-06 23:02:48 +0800

    测试代码相关2021.0106.2302

--- src/test/java/site/dunhanson/tablestore/spring/boot/TablestoreTest.java
+++ src/test/java/site/dunhanson/tablestore/spring/boot/TablestoreTest.java
@@ -1,14 +1,21 @@
 package site.dunhanson.tablestore.spring.boot;
 
+import cn.hutool.json.JSONUtil;
 import com.alicloud.openservices.tablestore.SyncClient;
-import com.alicloud.openservices.tablestore.model.Row;
+import com.alicloud.openservices.tablestore.model.*;
 import com.alicloud.openservices.tablestore.model.search.SearchQuery;
 import com.alicloud.openservices.tablestore.model.search.SearchRequest;
 import com.alicloud.openservices.tablestore.model.search.SearchResponse;
 import com.alicloud.openservices.tablestore.model.search.query.MatchQuery;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TablestoreTest {
 
@@ -16,21 +23,56 @@ public class TablestoreTest {
     public void testMatchQuery() {
         // Query
         MatchQuery query = new MatchQuery();
-        query.setFieldName("doctitle");
-        query.setText("档案");
+        query.setFieldName("docid");
+        query.setText("129226674");
+
         // SearchQuery
         SearchQuery searchQuery = new SearchQuery();
         searchQuery.setQuery(query);
         searchQuery.setGetTotalCount(true);
+
         // SearchRequest
         SearchRequest searchRequest = new SearchRequest("document", "document_index", searchQuery);
+        SearchRequest.ColumnsToGet columnsToGet = new SearchRequest.ColumnsToGet();
+        columnsToGet.setReturnAll(true);
+        searchRequest.setColumnsToGet(columnsToGet);
+
         // search
         SyncClient client = TestUtils.getSyncClient();
         SearchResponse response = client.search(searchRequest);
         System.out.println("totalCount:" + response.getTotalCount());
         List<Row> rows = response.getRows();
-        rows.forEach(obj->{
-            System.out.println(obj.getColumnsMap());
-        });
+        List<Map<String, Object>> list = new ArrayList<>();
+        for(Row row : rows) {
+            // 主键
+            PrimaryKey primaryKey = row.getPrimaryKey();
+            PrimaryKeyColumn[] primaryKeyColumns = primaryKey.getPrimaryKeyColumns();
+            for(int i = 0; i < primaryKeyColumns.length; i++) {
+                PrimaryKeyColumn primaryKeyColumn = primaryKeyColumns[i];
+                System.out.println(primaryKeyColumn.getName());
+            }
+            System.out.println();
+            // 字段
+            Column[] columns = row.getColumns();
+            Map<String, Object> map = new HashMap<>();
+            for(int i = 0; i < columns.length; i++) {
+                Column column = columns[i];
+                // 字段名
+                String name = column.getName();
+                // 字段值
+                ColumnValue columnValue = column.getValue();
+                // 如果是字符串判断是否是json，把json转换成List<Map<String, Object>>
+                if(columnValue.getType() == ColumnType.STRING) {
+                    String jsonStr = columnValue.asString();
+                    if(JSONUtil.isJson(jsonStr)) {
+                        Type type = new TypeToken<List<Map<String, Object>>>(){}.getType();
+                        map.put(name, new Gson().fromJson(jsonStr, type));
+                    }
+                } else {
+                    map.put(name, columnValue.getValue());
+                }
+            }
+            System.out.println();
+        }
     }
 }
